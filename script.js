d0f3b3fd661c9bb830a3ea2f5bd81b9fce7dfdd9
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2021-09-06 21:22:44 +0100

    Add menu toggle to mobile menu
    Add html elements for mobile menu
    Add style to our mobile menu elements
    Add js file for hamburger menu toggle

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-let 
+let menu = document.querySelector(".menu")
+let ham = document.querySelector(".ham")
+let xIcon = document.querySelector(".xIcon")
+let menuIcon = document.querySelector(".menuIcon")
+
+ham.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
