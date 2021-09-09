808250ee11f50a9e4c751f9689e86118820201fe
Bunbee <barnabmutinhiri@gmail.com>
2021-09-09 20:29:16 +0200

    Add script

--- script.js
+++ script.js
@@ -2,29 +2,26 @@ let menu = document.querySelector(".menu")
 let ham = document.querySelector(".ham")
 let xIcon = document.querySelector(".xIcon")
 let menuIcon = document.querySelector(".menuIcon")
-const main = document.querySelector('.main');
-// const logo = document.querySelector('.bold');
-const header = document.getElementById('logo');
-const projs = document.querySelectorAll('.work');
-
-function projOn() {
-  projs.forEach(
-    (work) => {
-      work.style.filter = 'blur(4px)';
-    },
-  );
-}
-
-function projOff() {
-  projs.forEach(
-    (work) => {
-      work.style.filter = 'blur(0px)';
-    },
-  );
-}
+const main =  document.querySelector('.main')
+const header = document.getElementById('logo')
+const projs = document.querySelectorAll('.work')
 
 
+// function projOn() {
+//   projs.forEach(
+//     (work) => {
+//       work.style.filter = 'blur(4px)';
+//     },
+//   );
+// }
 
+// function projOff() {
+//   projs.forEach(
+//     (work) => {
+//       work.style.filter = 'blur(0px)';
+//     },
+//   );
+// }
 
 const toggleMenu = () => {
   if (menu.classList.contains("showMenu")) {
@@ -48,6 +45,44 @@ menuLinks.forEach(
   }
 )
 
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+});
+
+overlay.addEventListener('click' , () =>{
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return 
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
 const modalObj = {
   modal1: {
     title: 'Tonic',
@@ -55,8 +90,8 @@ const modalObj = {
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     img: 'img/SnapshootPortfolio(1).png',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://chukwuebukavictor.github.io/My-portfolio/',
-    source: 'https://github.com/chukwuebukaVictor/My-portfolio',
+    live: 'https://mutinhiri.github.io/My-portfolio/',
+    source: 'https://github.com/mutinhiri/portfolio-setup',
   },
   modal2: {
     title: 'Multi-Post Stories',
@@ -64,17 +99,17 @@ const modalObj = {
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     img: 'img/SnapshootPortfolio(2).png',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://chukwuebukavictor.github.io/My-portfolio/',
-    source: 'https://github.com/chukwuebukaVictor/My-portfolio',
+    live: 'https://mutinhiri.github.io/My-portfolio/',
+    source: 'https://github.com/mutinhiri/portfolio-setup',
   },
   modal3: {
     title: 'Tonic',
     subt: ['CANOPY', 'Back End Dev', '2015'],
-    desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: 'img/SnapshootPortfolio(3).png',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://chukwuebukavictor.github.io/My-portfolio/',
-    source: 'https://github.com/chukwuebukaVictor/My-portfolio',
+    live: 'https://mutinhiri.github.io/My-portfolio/',
+    source: 'https://github.com/mutinhiri/portfolio-setup',
   },
   modal4: {
     title: 'Multi-Post Stories',
@@ -82,54 +117,52 @@ const modalObj = {
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     img: 'img/SnapshootPortfolio(4).png',
     tech: ['html', 'css', 'javaScript'],
-    live: 'https://chukwuebukavictor.github.io/My-portfolio/',
-    source: 'https://github.com/chukwuebukaVictor/My-portfolio',
+    live: 'https://mutinhiri.github.io/My-portfolio/',
+    source: 'https://github.com/mutinhiri/portfolio-setup',
   },
 };
 
-const modalSubt = document.getElementById('modalSubt');
-const modalTech = document.getElementById('modalTech');
+// const modalSubt = document.getElementById('modalSubt');
+// const modalTech = document.getElementById('modalTech');
 
-function addCounter() {
-  const counter = document.createElement('img');
-  counter.src = 'img/Counter.svg';
-  counter.alt = 'counter';
-  modalSubt.appendChild(counter);
-}
+// // // function addCounter() {
+// //   const counter = document.createElement('img');
+// //   counter.src = 'img/Counter.svg';
+// //   counter.alt = 'counter';
+// //   modalSubt.appendChild(counter);
+// // }
 
-function addSubt(modal) {
-  const firstDiv = document.createElement('div');
-  // firstDiv.className = 'font500 black font18-24';
-  modalSubt.appendChild(firstDiv);
-  [firstDiv.innerHTML] = modalObj[modal].subt;
-  modalObj[modal].subt.slice(1).forEach((i) => {
-    addCounter();
-    const otherDiv = document.createElement('div');
-    // otherDiv.className = 'gray font18-24';
-    modalSubt.appendChild(otherDiv);
-    otherDiv.innerHTML = i;
-  });
-}
+// function addSubt(modal) {
+//   const firstDiv = document.createElement('div');
+//   firstDiv.className = 'font500 black font18-24';
+//   modalSubt.appendChild(firstDiv);
+//   [firstDiv.innerHTML] = modalObj[modal].subt;
+//   modalObj[modal].subt.slice(1).forEach((i) => {
+//     addCounter();
+//     const otherDiv = document.createElement('div');
+//     otherDiv.className = 'gray font18-24';
+//     modalSubt.appendChild(otherDiv);
+//     otherDiv.innerHTML = i;
+//   });
+// }
 
-function addTechs(modal) {
-  modalObj[modal].tech.forEach((i) => {
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(i));
-    modalTech.appendChild(li);
-  });
-}
+// function addTechs(modal) {
+//   modalObj[modal].tech.forEach((i) => {
+//     const li = document.createElement('li');
+//     li.appendChild(document.createTextNode(i));
+//     modalTech.appendChild(li);
+//   });
+// }
 
-function clearModal() {
-  modalTech.innerHTML = '';
-  modalSubt.innerHTML = '';
-}
+// function clearModal() {
+//   modalTech.innerHTML = '';
+//   modalSubt.innerHTML = '';
+// }
 
 function popModal(modal) {
-  document.getElementById('modalTitle').textContent = modalObj[modal].title;
-  addSubt(modal);
-  document.getElementById('modalDesc').textContent = modalObj[modal].desc;
-  addTechs(modal);
-  document.getElementById('modalImg').src = modalObj[modal].img;
+  document.getElementById('modalTitle').innerHTML = modalObj[modal].title;
+  document.getElementById('modalDesc').innerHTML = modalObj[modal].desc;
+  document.getElementById('modalImg').setAttribute("src", modalObj[modal].img);
   document.getElementById('modalBtn1').setAttribute('onClick', `location.href='${modalObj[modal].live}';`);
   document.getElementById('modalBtn2').setAttribute('onClick', `location.href='${modalObj[modal].source}';`);
 }
@@ -139,55 +172,59 @@ const mBtn1 = document.getElementById('myBtn1');
 const mBtn2 = document.getElementById('myBtn2');
 const mBtn3 = document.getElementById('myBtn3');
 const mBtn4 = document.getElementById('myBtn4');
-const xp = document.getElementsByClassName('xp')[0];
+// const xp = document.getElementsByClassName('xp')[0];
 
 mBtn1.onclick = function seeP1() {
   popModal('modal1');
-  modal.style.display = 'block';
-  main.style.filter = 'blur(4px)';
-  header.style.filter = 'blur(4px)';
-  projOn();
-};
+}
+//   modal.style.display = 'block';
+//   main.style.filter = 'blur(4px)';
+//   header.style.filter = 'blur(4px)';
+//   projOn();
+// };
 
 mBtn2.onclick = function seeP2() {
   popModal('modal2');
-  modal.style.display = 'block';
-  main.style.filter = 'blur(4px)';
-  header.style.filter = 'blur(4px)';
-  projOn();
-};
+}
+//   modal.style.display = 'block';
+//   main.style.filter = 'blur(4px)';
+//   header.style.filter = 'blur(4px)';
+//   projOn();
+// };
 
 mBtn3.onclick = function seeP3() {
   popModal('modal3');
-  modal.style.display = 'block';
-  main.style.filter = 'blur(4px)';
-  header.style.filter = 'blur(4px)';
-  projOn();
-};
+}
+//   modal.style.display = 'block';
+//   main.style.filter = 'blur(4px)';
+//   header.style.filter = 'blur(4px)';
+//   projOn();
+// };
 
 mBtn4.onclick = function seeP4() {
   popModal('modal4');
-  modal.style.display = 'block';
-  main.style.filter = 'blur(4px)';
-  header.style.filter = 'blur(4px)';
-  projOn();
-};
-
-function closeModal() {
-  modal.style.display = 'none';
-  main.style.filter = 'blur(0px)';
-  header.style.filter = 'blur(0px)';
-  projOff();
 }
+//   modal.style.display = 'block';
+//   main.style.filter = 'blur(4px)';
+//   header.style.filter = 'blur(4px)';
+//   projOn();
+// };
 
-xp.onclick = function x() {
-  closeModal();
-  clearModal();
-};
+// function closeModal() {
+//   modal.style.display = 'none';
+//   main.style.filter = 'blur(0px)';
+//   header.style.filter = 'blur(0px)';
+//   projOff();
+// }
 
-window.onclick = function clickOut(event) {
-  if (event.target === modal) {
-    closeModal();
-    clearModal();
-  }
-};
+// xp.onclick = function x() {
+//   closeModal();
+//   clearModal();
+// };
+
+// window.onclick = function clickOut(event) {
+//   if (event.target === modal) {
+//     closeModal();
+//     clearModal();
+//   }
+// };
