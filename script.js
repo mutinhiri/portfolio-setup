963c4b32ff729344b135cc252bc0893056101c85
Bunbee <barnabmutinhiri@gmail.com>
2021-09-10 10:25:04 +0200

    Add changes

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button]');
 const overlay = document.getElementById('overlay');
 
-openModalButtons.forEach(button => {
+openModalButtons.forEach(button  => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
