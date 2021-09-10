5b945a059abc6e063cff5a9505e0c54328efcc2f
Bunbee <barnabmutinhiri@gmail.com>
2021-09-10 22:10:24 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const modalObj = {
   modal2: {
     title: 'Multi-Post Stories',
     desc: "Lorem Ipsum is simply dummy text of the printinhe industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially",
-    subt: ['CANOPY', 'Back End Dev', ],
+    subt: ['CANOPY', 'Back End Dev'],
     img: 'img/SnapshootPortfolio(2).png',
     tech: ['html', 'css', 'javaScript'],
     live: 'https://mutinhiri.github.io/My-portfolio/',
